14dec55c04fc338f6520b69ec4b8969701270132
Andrea Veri <averi@redhat.com>
2018-03-23 13:15:23 +0100

    Land the additional contributors on the PKG-INFO metadata file

--- setup.py
+++ setup.py
@@ -12,15 +12,15 @@ any imaging libraries because the CAPTCHA is served directly from reCAPTCHA.
 Also allows you to securely obfuscate emails with Mailhide. This functionality
 requires pycrypto. This library requires two types of API keys. If you'd like
 to use the CAPTCHA, you'll need a key from https://www.google.com/recaptcha/admin/create.
-For Mailhide, you'll need a key from http://www.google.com/recaptcha/mailhide/apikey.
+For Mailhide, you'll need a key from https://www.google.com/recaptcha/mailhide/apikey.
 
-There is also a Google Group at http://groups.google.com/group/recaptcha/.
+There is also a Google Group at https://groups.google.com/group/recaptcha/.
 Please use the associated mailing list for any questions or comments:
 recaptcha@googlegroups.com. Like the Google Code project, the Google Group
 mailing list is also shared among the several reCAPTCHA client implementations.
 
-reCAPTCHA is written by Ben Maurer and maintained by Josh Bronson and Richard Monk. It is
-licensed under an MIT/X11 license.
+reCAPTCHA is written by Ben Maurer and maintained by Josh Bronson, Richard Monk and
+Andrea Veri. It is licensed under an MIT/X11 license.
 """,
 
       license = "MIT/X11",
